e568ca54b4c2c5d9859dcf865794092644ceefe7
Manish Ray <manishray2233@gmail.com>
2024-02-05 17:18:08 +0530

    assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import { Room } from "./Room";
-
 export default function Page() {
-  return (
-    <Room>
-      <h1></h1>
-    </Room>
-  );
+  return (<h1 className="text-5xl text-white">
+    Liveblocks Figma Clone
+  </h1>);
 }
