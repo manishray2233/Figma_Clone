848b125c285c7fd47080f0afde6689acb73e9030
Manish Ray <manishray2233@gmail.com>
2024-02-20 23:00:22 +0530

    done with realtime features

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,16 @@ import { useEffect, useRef, useState } from "react";
 import {
   handleCanvasMouseDown,
   handleCanvasMouseMove,
+  handleCanvasMouseUp,
+  handleCanvasObjectModified,
   handleResize,
   initializeFabric,
+  renderCanvas,
 } from "@/lib/canvas";
 import { ActiveElement } from "@/types/type";
 import { useMutation, useStorage } from "@/liveblocks.config";
+import { handleDelete } from "@/lib/key-events";
+import { defaultNavElement } from "@/constants";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -21,6 +26,7 @@ export default function Page() {
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<String | null>("circle");
+  const activeObjectRef = useRef<fabric.Object | null>(null);
 
   const canvasObjects = useStorage((root) => root.canvasObjects);
 
@@ -43,32 +49,172 @@ export default function Page() {
     icon: "",
   });
 
+  const deleteAllShapes = useMutation(({ storage }) => {
+    // get the canvasObjects store
+    const canvasObjects = storage.get("canvasObjects");
+
+    // if the store doesn't exist or is empty, return
+    if (!canvasObjects || canvasObjects.size === 0) return true;
+
+    // delete all the shapes from the store
+    for (const [key, value] of canvasObjects.entries()) {
+      canvasObjects.delete(key);
+    }
+
+    // return true if the store is empty
+    return canvasObjects.size === 0;
+  }, []);
+
+   /**
+   * deleteShapeFromStorage is a mutation that deletes a shape from the
+   * key-value store of liveblocks.
+   * useMutation is a hook provided by Liveblocks that allows you to perform
+   * mutations on liveblocks data.
+   *
+   * useMutation: https://liveblocks.io/docs/api-reference/liveblocks-react#useMutation
+   * delete: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap.delete
+   * get: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap.get
+   *
+   * We're using this mutation to delete a shape from the key-value store when
+   * the user deletes a shape from the canvas.
+   */
+   const deleteShapeFromStorage = useMutation(({ storage }, shapeId) => {
+    /**
+     * canvasObjects is a Map that contains all the shapes in the key-value.
+     * Like a store. We can create multiple stores in liveblocks.
+     *
+     * delete: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap.delete
+     */
+    const canvasObjects = storage.get("canvasObjects");
+    canvasObjects.delete(shapeId);
+  }, []);
+
   const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement(elem);
+
+    switch (elem?.value) {
+      // delete all the shapes from the canvas
+      case "reset":
+        // clear the storage
+        deleteAllShapes();
+        // clear the canvas
+        fabricRef.current?.clear();
+        // set "select" as the active element
+        setActiveElement(defaultNavElement);
+        break;
+
+      // delete the selected shape from the canvas
+      case "delete":
+        // delete it from the canvas
+        handleDelete(fabricRef.current as any, deleteShapeFromStorage);
+        // set "select" as the active element
+        setActiveElement(defaultNavElement);
+        break;
+
+      // upload an image to the canvas
+      case "image":
+        // trigger the click event on the input element which opens the file dialog
+        imageInputRef.current?.click();
+        /**
+         * set drawing mode to false
+         * If the user is drawing on the canvas, we want to stop the
+         * drawing mode when clicked on the image item from the dropdown.
+         */
+        isDrawing.current = false;
+
+        if (fabricRef.current) {
+          // disable the drawing mode of canvas
+          fabricRef.current.isDrawingMode = false;
+        }
+        break;
+
+      // for comments, do nothing
+      case "comments":
+        break;
+
+      default:
+        // set the selected shape to the selected element
+        selectedShapeRef.current = elem?.value as string;
+        break;
+    }
+  };
 
     selectedShapeRef.current = elem?.value as string;
   };
 
   useEffect(() => {
-    const canvas = initializeFabric({ canvasRef, fabricRef });
+    // initialize the fabric canvas
+    const canvas = initializeFabric({
+      canvasRef,
+      fabricRef,
+    });
 
+    /**
+     * listen to the mouse down event on the canvas which is fired when the
+     * user clicks on the canvas
+     *
+     * Event inspector: http://fabricjs.com/events
+     * Event list: http://fabricjs.com/docs/fabric.Canvas.html#fire
+     */
     canvas.on("mouse:down", (options) => {
       handleCanvasMouseDown({
         options,
         canvas,
+        selectedShapeRef,
         isDrawing,
         shapeRef,
-        selectedShapeRef,
       });
     });
 
+    /**
+     * listen to the mouse move event on the canvas which is fired when the
+     * user moves the mouse on the canvas
+     *
+     * Event inspector: http://fabricjs.com/events
+     * Event list: http://fabricjs.com/docs/fabric.Canvas.html#fire
+     */
     canvas.on("mouse:move", (options) => {
       handleCanvasMouseMove({
         options,
         canvas,
         isDrawing,
-        shapeRef,
         selectedShapeRef,
+        shapeRef,
+        syncShapeInStorage,
+      });
+    });
+
+    /**
+     * listen to the mouse up event on the canvas which is fired when the
+     * user releases the mouse on the canvas
+     *
+     * Event inspector: http://fabricjs.com/events
+     * Event list: http://fabricjs.com/docs/fabric.Canvas.html#fire
+     */
+    canvas.on("mouse:up", () => {
+      handleCanvasMouseUp({
+        canvas,
+        isDrawing,
+        shapeRef,
+        activeObjectRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+        setActiveElement,
+      });
+    });
+
+     /**
+     * listen to the object modified event on the canvas which is fired
+     * when the user modifies an object on the canvas. Basically, when the
+     * user changes the width, height, color etc properties/attributes of
+     * the object or moves the object on the canvas.
+     *
+     * Event inspector: http://fabricjs.com/events
+     * Event list: http://fabricjs.com/docs/fabric.Canvas.html#fire
+     */
+     canvas.on("object:modified", (options) => {
+      handleCanvasObjectModified({
+        options,
         syncShapeInStorage,
       });
     });
@@ -78,7 +224,19 @@ export default function Page() {
         canvas: fabricRef.current,
       });
     });
+
+    return () => {
+      canvas.dispose(); 
+    };
   }, []);
+
+  useEffect(()=>{
+    renderCanvas({
+      fabricRef,
+      canvasObjects,
+      activeObjectRef
+    })
+  },[canvasObjects])
 
   return (
     <main className="h-screen overflow-hidden">
